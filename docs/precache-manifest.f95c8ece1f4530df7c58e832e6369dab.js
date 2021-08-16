091af615fbc8eb36bab04c6cdbc7049ad2c5dc2e
mexparedes <maxi.paredes.r@gmail.com>
2021-08-16 00:15:21 -0400

    docs agregada

---
+++ docs/precache-manifest.f95c8ece1f4530df7c58e832e6369dab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fb31ab648b08327aaf68fe6a410543d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "618ce21f345aaa66171d",
+    "url": "/static/css/main.3dad3556.chunk.css"
+  },
+  {
+    "revision": "e107496e2e01402b74e7",
+    "url": "/static/js/2.b11609f6.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.b11609f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "618ce21f345aaa66171d",
+    "url": "/static/js/main.216c4bcc.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
